271d48e285a17fc0a495045abeb93c90da734119
Jess <jlcasasg@unal.edu.co>
2019-09-21 19:15:47 -0500

    Add first unit tests

--- src/app/components/display/display.component.spec.ts
+++ src/app/components/display/display.component.spec.ts
@@ -24,4 +24,33 @@ describe('DisplayComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render current value as 0', async() => {
+    component.currentValue = '0';
+    fixture.detectChanges();
+    let compontent_ne = fixture.debugElement.nativeElement;
+    expect(compontent_ne.querySelector('.displayValue').textContent).toContain('0');
+  });
+
+  it('should render current value as 1+2', async() => {
+    component.currentValue = '1+2';
+    fixture.detectChanges();
+    let compontent_ne = fixture.debugElement.nativeElement;
+    expect(compontent_ne.querySelector('.displayValue').textContent).toContain('1+2');
+  });
+
+  it('should render current value as 0', async() => {
+    component.currentValue = null;
+    fixture.detectChanges();
+    let compontent_ne = fixture.debugElement.nativeElement;
+    expect(compontent_ne.querySelector('.displayValue').textContent).toEqual('');
+  });
+
+  
+  it('should render current value as 0', async() => {
+    component.currentValue = undefined;
+    fixture.detectChanges();
+    let compontent_ne = fixture.debugElement.nativeElement;
+    expect(compontent_ne.querySelector('.displayValue').textContent).toEqual('');
+  });
 });
