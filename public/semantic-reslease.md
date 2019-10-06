91335f8b803f05c3c973d4ed163c65addb860ba4
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2019-10-06 10:56:47 -0500

    feat(sematic): Add semantic installation

---
+++ public/semantic-reslease.md
@@ -0,0 +1,5 @@
+
+
+See more in [https://github.com/semantic-release/semantic-release/](https://github.com/semantic-release/semantic-release/)
+GITHUB_TOKEN=b29933c83f69ff0d7266bea848a03352bf5e0d20
+GH_TOKEN=b29933c83f69ff0d7266bea848a03352bf5e0d20
---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branch: 'feature/semantic-release2',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+    //   '@semantic-release/npm',
+    //   [
+    //     '@semantic-release/exec',
+    //     {
+    //       prepareCmd: 'oclif-dev manifest && oclif-dev readme'
+    //     }
+    //   ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['README.md'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+      ],
+      '@semantic-release/github'
+    ]
+  }
