5a4f92a84ed2a852ebcb0e3331d29c13cc499065
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2019-10-06 12:05:16 -0500

    Add two branches

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branch: "master",
+  branch: ["master", "feature/semantic-release"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
