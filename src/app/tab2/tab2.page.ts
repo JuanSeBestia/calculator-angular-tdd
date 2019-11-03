0e79a5c48107590861d67613bd4d5279484ed43a
Jess <jlcasasg@unal.edu.co>
2019-11-02 22:49:57 -0500

    add calculator service

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
   displayValue: string;
-  validInput = '^((\-)?(\d+\.?\d*)+([\+\-x\/]{1}(\d+\.?\d*)*)*)+';
 
   constructor() {
     this.displayValue = '0';
