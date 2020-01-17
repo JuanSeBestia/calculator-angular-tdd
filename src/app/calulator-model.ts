3f3dd442083d80fd687b4cc993e225d9d9d6ea23
Jess Github <jlcasasg@unal.edu.co>
2020-01-17 15:11:24 -0500

    feat: Add pagination to math-operations table

--- src/app/calulator-model.ts
+++ src/app/calulator-model.ts
@@ -1,4 +1,12 @@
 export interface CalculatorModel {
+    data: Array<CalculatorDataModel>,
+    totalPages: number,
+    currentPage: number,
+    pageSize: number,
+    count: number,
+}
+
+export interface CalculatorDataModel {
     username: string,
     date: Date,
     result: string,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true
 };
+export const BASE_URL = 'http://localhost:3000';
