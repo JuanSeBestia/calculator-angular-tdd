4fb117656dbf9828e2c420404bf04809b63c4a15
Jess Github <jlcasasg@unal.edu.co>
2020-01-21 14:11:30 -0500

    Style (US-2) fix lint

--- src/app/http-interceptors/myInterceptor.ts
+++ src/app/http-interceptors/myInterceptor.ts
@@ -1,24 +1,24 @@
-import { Injectable } from "@angular/core";
-import { tap } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import {
   HttpRequest,
   HttpHandler,
   HttpInterceptor,
   HttpHeaders,
-} from "@angular/common/http";
+} from '@angular/common/http';
 
 @Injectable()
 
-export class myInterceptor implements HttpInterceptor {
-    constructor(){
+export class MyInterceptor implements HttpInterceptor {
+    constructor() {
     }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler){
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
         const headers = new HttpHeaders();
         headers.set('Content-Type', 'application/json');
 
         const newReq = req.clone({
-            headers: headers,
+            headers,
         });
 
         return next.handle(newReq);
