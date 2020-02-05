7966a2ffbe84e67773b9a46ef29176bba3007f14
Jess Github <jlcasasg@unal.edu.co>
2020-02-05 16:13:42 -0500

    :white_check_mark: test: Add Interceptor Test

--- src/app/http-interceptors/http-interceptor.spec.ts
+++ src/app/http-interceptors/http-interceptor.spec.ts
@@ -1,7 +1,79 @@
 import { MyInterceptor } from './myInterceptor';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RequestService } from '../request.service';
+import { HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+
 
 describe('HttpInterceptor', () => {
+  let service: RequestService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        RequestService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MyInterceptor,
+          multi: true,
+        }
+      ]
+    });
+
+    service = TestBed.get(RequestService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
   it('should create an instance', () => {
     expect(new MyInterceptor()).toBeTruthy();
   });
+
+  it('should add a Content-Type header', () => {
+    service.getMathOperationsList().subscribe(); // Just call the method
+
+    const httpRequest = httpMock.expectOne('https://calculator-node-tdd.herokuapp.com/mathOperation');
+
+    expect(httpRequest.request.headers.has('Content-Type')).toEqual(true);
+  });
+
+  it('should handle error', () => {
+    const interceptor = new MyInterceptor();
+    // "as any" to avoid the "private method" type errors
+    spyOn(interceptor as any, 'handleError');
+    const mockErrorResponse = {
+      status: 400,
+      statusText: 'Unknown Error'
+    };
+
+    service.getMathOperationsList().subscribe(
+      () => {
+        // Nothing
+      },
+      (error) => {
+        expect(interceptor.handleError).toHaveBeenCalled();
+      }
+    );
+    const request = httpMock.expectOne('https://calculator-node-tdd.herokuapp.com/mathOperation');
+    request.flush(mockErrorResponse);
+  });
+
+
+  it('should call presentAlertError error', () => {
+    const interceptor = new MyInterceptor();
+    // "as any" to avoid the "private method" type errors
+    spyOn(interceptor as any, 'presentErrorAlert').and.callThrough();
+    const mockErrorResponse = new HttpErrorResponse({
+      error: 'error',
+      status: 400,
+      statusText: 'Unknown Error',
+    });
+
+    interceptor.handleError(mockErrorResponse);
+
+    expect(interceptor.presentErrorAlert).toHaveBeenCalledWith('Backend Error error. Error code 400');
+
+  });
+
 });
