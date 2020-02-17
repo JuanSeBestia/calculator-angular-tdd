0c4a6b9092c89c3527700dfc3df928741aa9ce36
Jess Github <jlcasasg@unal.edu.co>
2020-02-17 13:50:01 -0500

    refactor(calculatorService, display, tab2page): :recycle: test with obs.
    Redefine test with now the use of observable

---
+++ src/app/calculator/calculator.service.spec.ts
@@ -0,0 +1,83 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalculatorService } from './calculator.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { appReducers } from '../store/reducers/app.reducers';
+import { StoreModule, Store } from '@ngrx/store';
+import { AddOperatorValue, SetResultValue, ClearValue, UpdateUserName } from '../store/actions/calculator.actions';
+import { CalculatorState } from '../store/state/calculator.state';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { AppState } from '../store/state/app.state';
+
+describe('CalculatorService', () => {
+  let store: MockStore<{
+    operations: null,
+    currentOperation: {
+      math_operation: '',
+      result: '',
+      username: '',
+    }
+  }>;
+  const initialState = {
+    operations: null,
+    currentOperation: {
+      math_operation: '1+1',
+      result: '2',
+      username: 'Test',
+    }
+  }
+  let service: CalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule, StoreModule.forRoot(appReducers),] })
+
+    store = TestBed.get<Store<AppState>>(Store);
+  });
+
+  it('should be created', () => {
+    service = TestBed.get(CalculatorService);
+    expect(service).toBeTruthy();
+  });
+
+
+  it('should disptach AddOperatorAction with 1', () => {
+    const expectedAction = new AddOperatorValue('1');
+    const store = jasmine.createSpyObj<Store<AppState>>('store', ['dispatch']);
+
+    const service = new CalculatorService(store);
+    service.setCurrentValue('1');
+
+    expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+  });
+
+  it('should disptach SetResultValue', () => {
+    const expectedAction = new SetResultValue();
+    const store = jasmine.createSpyObj<Store<AppState>>('store', ['dispatch']);
+
+    const service = new CalculatorService(store);
+    service.getResult();
+
+    expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+  });
+
+  it('should disptach ClearValue', () => {
+    const expectedAction = new ClearValue();
+    const store = jasmine.createSpyObj<Store<AppState>>('store', ['dispatch']);
+
+    const service = new CalculatorService(store);
+    service.clean();
+
+    expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+  });
+
+  it('should disptach UpdateUsername', () => {
+    const expectedAction = new UpdateUserName('Test');
+    const store = jasmine.createSpyObj<Store<AppState>>('store', ['dispatch']);
+
+    const service = new CalculatorService(store);
+    service.updateUsername('Test');
+
+    expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+  });
+
+});
