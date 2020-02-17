fb5c43353411fd2fccf3da986a2453200e24cc0e
Jess Github <jlcasasg@unal.edu.co>
2020-02-17 13:50:01 -0500

    refactor(calculatorService, display, tab2page): :recycle: test with obs.
    Redefine test with now the use of observable

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -9,6 +9,9 @@ import { CleanBtnComponent } from '../calculator/components/clean-btn/clean-btn.
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from '../store/reducers/app.reducers';
+import { CalculatorDataModel } from '../calculator/models/calulator-model';
 
 
 describe('Tab2Page', () => {
@@ -18,8 +21,14 @@ describe('Tab2Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab2Page, DisplayComponent, NumberButtonComponent, ResultBtnComponent,
-        CleanBtnComponent, ],
-      imports: [IonicModule.forRoot(), CommonModule, FormsModule, HttpClientTestingModule]
+        CleanBtnComponent,],
+      imports: [
+        IonicModule.forRoot(),
+        CommonModule,
+        FormsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot(appReducers),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab2Page);
@@ -28,33 +37,40 @@ describe('Tab2Page', () => {
   }));
 
   it('should create', () => {
-
     expect(component).toBeTruthy();
   });
 
-  it('displayValue should exist', () => {
-    expect(component.displayValue).toBeDefined();
+  it('mathOperation should not contain \'undefined\'', (done) => {
+    component.displayValue$.subscribe((calcModel: CalculatorDataModel) => {
+      expect(calcModel.math_operation).not.toContain('undefined');
+      done();
+    });
   });
 
 
-  it('displayValue should not contain \'undefined\'', () => {
-    expect(component.displayValue).not.toContain('undefined');
+  it('mathOperation should be an empty string', (done) => {
+    component.displayValue$.subscribe((calcModel: CalculatorDataModel) => {
+      expect(calcModel.math_operation.length).toEqual(0);
+      done();
+    });
   });
 
-  it('displayValue should be an empty string', () => {
-    expect(component.displayValue.length).toEqual(0);
-  });
 
-
-  it('after clicking \'=\' displayValue should not contain \'undefined\'', () => {
+  it('after clicking \'=\' mathOperation should not contain \'undefined\'', (done) => {
     component.displayResult();
-    expect(component.displayValue).not.toContain('undefined');
+    component.displayValue$.subscribe((calcModel: CalculatorDataModel) => {
+      expect(calcModel).not.toContain('undefined');
+      done();
+    });
   });
 
-  it('after clicking \'=\' after clicking a math expression displayValue should not contain \'undefined\'', () => {
+  it('after clicking \'=\' after clicking a math expression, mathOperation should not contain \'undefined\'', (done) => {
     component.updateDisplayValue('2+2');
     component.displayResult();
-    expect(component.displayValue).not.toContain('undefined');
+    component.displayValue$.subscribe((calcModel: CalculatorDataModel) => {
+      expect(calcModel.math_operation).not.toContain('undefined');
+      done();
+    });
   });
 
   it('should have a Background image', () => {
@@ -64,15 +80,16 @@ describe('Tab2Page', () => {
 
 
   it('should show a demon image when the value of calculator is "666"', () => {
-    component.displayValue = '666';
+    component.updateDisplayValue('666');
     fixture.detectChanges();
     const element = document.querySelector('.background__image') as HTMLElement;
     expect(element.classList).toContain('demon-background');
   });
 
 
+
   it('should show a God image when the value of calculator is "42"', () => {
-    component.displayValue = '42';
+    component.updateDisplayValue('42');
     fixture.detectChanges();
     const element = document.querySelector('.background__image') as HTMLElement;
     expect(element.classList).toContain('god-background');
